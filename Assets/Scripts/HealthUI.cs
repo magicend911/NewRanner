d133fd5d98e4af96f07e3b9881526e4601fbc69d
Ferzov Anton <magicend911@outlook.com>
2024-12-01 16:49:07 +0700

    add a little changes

--- Assets/Scripts/HealthUI.cs
+++ Assets/Scripts/HealthUI.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 
 public class HealthUI : MonoBehaviour
 {
-    [SerializeField] private Player playerHealth; // Ссылка на Player
-    [SerializeField] private TextMeshProUGUI healthText; // Текст для отображения жизней
+    [SerializeField] private Player player; 
+    [SerializeField] private TextMeshProUGUI healthText;
 
-    void Start()
+    private void OnEnable()
     {
-        playerHealth.OnHealthChanged += UpdateHealthUI;
-        UpdateHealthUI(playerHealth.MaxHealth); // Отобразить стартовые жизни
+        player.HealthChanged += onHealthChanged;
+    }
+    private void OnDisable()
+    {
+        player.HealthChanged -= onHealthChanged;
     }
 
-    private void UpdateHealthUI(int currentHealth)
+    private void onHealthChanged(int health)
     {
-        healthText.text = "Lives: " + currentHealth;
+        healthText.text = health.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,54 +1,32 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 3;
     public int MaxHealth => maxHealth;
     private int currentHealth;
-
-    // Событие для отображения жизней или завершения игры
-    public delegate void HealthChanged(int currentHealth);
-    public event HealthChanged OnHealthChanged;
+   
+    public event UnityAction<int> HealthChanged;
+    public event UnityAction Died;
 
     void Start()
     {
-        // Устанавливаем начальное количество жизней
         currentHealth = maxHealth;
-
-        // Уведомляем подписчиков о начальном состоянии
-        OnHealthChanged?.Invoke(currentHealth);
+        HealthChanged?.Invoke(currentHealth);
     }
-
-    // Метод для уменьшения жизней
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ограничиваем в пределах 0 и maxHealth
+        HealthChanged?.Invoke(currentHealth);
 
-        // Уведомляем подписчиков
-        OnHealthChanged?.Invoke(currentHealth);
-
-        // Проверяем, жив ли игрок
         if (currentHealth <= 0)
         {
             Die();
         }
     }
-
-    // Обработка смерти игрока
     private void Die()
     {
-        Debug.Log("Игрок погиб!");
-        // Можно добавить логику перезапуска уровня или завершения игры
-    }
-
-    // Восстановление здоровья (опционально)
-    public void Heal(int amount)
-    {
-        currentHealth += amount;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-
-        // Уведомляем подписчиков
-        OnHealthChanged?.Invoke(currentHealth);
+        Died?.Invoke();
     }
 }
